0f85a003af33eb69950eabce7a5ab0072790743e
Chuck J Hardy <chuck@generalassemb.ly>
2017-05-28 17:57:13 +0100

    Simple Detect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const record = require('node-record-lpcm16');
 
 // Imports the Google Cloud client library
@@ -19,15 +20,50 @@ const request = {
   config: {
     encoding: encoding,
     sampleRateHertz: sampleRateHertz,
-    languageCode: languageCode
+    languageCode: languageCode,
+    speechContexts: {
+      phrases: [
+        'Heroku'
+      ]
+    }
   },
   interimResults: false // If you want interim results, set this to true
 };
 
+const dictionary = {
+  'New Relic': {
+    'explain': 'Server Monitoring Tool',
+    'url': 'https://newrelic.com/about',
+    'mentions': 0
+  },
+  'AWS': {
+    'explain': 'Hosting Platform',
+    'url': 'https://aws.amazon.com/what-is-aws/',
+    'mentions': 0
+  },
+  'Heroku': {
+    'explain': 'Platform as a Service',
+    'url': 'https://www.heroku.com/what',
+    'mentions': 0
+  }
+}
+
+const detector = (data) => {
+  let results = data.results;
+  _.mapKeys(dictionary, function(value, key) {
+    if (results.indexOf(key) > -1) {
+      let bingo = dictionary[key];
+      let str = '-> ' + key + ': ' + bingo.explain + ' ' + bingo.url + '\n'
+      process.stdout.write(str);
+    }
+  });
+  // process.stdout.write(data.results);
+}
+
 // Create a recognize stream
 const recognizeStream = speech.createRecognizeStream(request)
   .on('error', console.error)
-  .on('data', (data) => process.stdout.write(data.results));
+  .on('data', detector);
 
 // Start recording and send the microphone input to the Speech API
 record
